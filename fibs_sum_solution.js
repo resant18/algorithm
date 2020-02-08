3613c4e3a8e56068c67d54c79e630912b3f58f9e
resant18 <santoso.renata@gmail.com>
2020-02-08 15:37:41 -0800

    Add time complexity to Fibonacci solution

--- fibs_sum_solution.js
+++ fibs_sum_solution.js
@@ -1,9 +1,13 @@
 // Write a function, `fibsSum(n)`, that finds the sum of the first n
 // fibonacci numbers recursively. Assume n > 0.
 // Note that for this problem, the fibonacci sequence starts with [1, 1]. 
+
+// Time: O(2^n)
 function fibsSum(n) {
   if (n === 0) return 0;
   if (n === 1) return 1;
 
   return fibsSum(n - 1) + fibsSum(n - 2) + 1;
 }
+
+console.log(fibsSum(3));
