8c7f57d75c5a855a3b5e979c9f8fdf0c71d3acdc
resant18 <santoso.renata@gmail.com>
2020-04-03 15:39:36 -0700

    Add merge two sorted linked list

--- merge_two_sorted_linkedlist.js
+++ merge_two_sorted_linkedlist.js
@@ -33,8 +33,26 @@
  * @return {ListNode}
  */
 
- var mergeTwoList = function(l1, l2) {
-   let head = new ListNode(null);
+  var mergeTwoList = function(l1, l2) {
+    let head = new ListNode(null);
+    let curr = head;
 
+    while (l1 && l2) {
+       if (l1.val < l2.val) {
+          curr.next = l1;
+          l1 = l1.next;
+       } else {
+          curr.next = l2;
+          l2 = l2.next;
+       }
+
+       curr = curr.next;
+    }
+
+    curr.next = l1 || l2;
+
+    return head.next;
+
+ 
 
  }
