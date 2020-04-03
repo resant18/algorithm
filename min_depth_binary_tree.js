7b181e12d2a07252107f20d8ffc830b07af0e907
resant18 <santoso.renata@gmail.com>
2020-04-02 22:55:18 -0700

    Add comment

--- min_depth_binary_tree.js
+++ min_depth_binary_tree.js
@@ -1,6 +1,5 @@
 // Leet code #111
 // Given a binary tree, find its minimum depth.
-
 // The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 
 // Note: A leaf is a node with no children.
@@ -38,14 +37,19 @@ node20.right = node7;
 // If right is null, return min depth of left side.
 
 var minDepth = function(root) { 
-   if (!root) return 0;
-
-   let minLeft = minDepth(root.left);
-   let minRight = minDepth(root.right);
-
-   if (!root.left) return minDepth(root.right) + 1;
-   if (!root.right) return minDepth(root.left) + 1;
-   return Math.min(minLeft, minRight) + 1;
+   //Null node has 0 depth
+    if (!root) return 0;            
+    
+    let minRight = minDepth(root.right);
+    let minLeft = minDepth(root.left);
+    
+    // if the node has no left sub-tree, then recur on the right sub-tree 
+    if (!root.left) return minRight + 1;        
+    // if the node has no right sub-tree, then recur on the left sub-tree
+    if (!root.right) return minLeft + 1;
+    
+    // if the node has right and left, find the minimum depth
+    return Math.min(minLeft, minRight) + 1;
 }
 
 console.log(minDepth(node3));
