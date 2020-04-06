975c637460c4ff0fc7b19c3f02ce5612c77ee1e8
resant18 <santoso.renata@gmail.com>
2020-04-05 22:51:51 -0700

    Add same tree

---
+++ same_tree.js
@@ -0,0 +1,20 @@
+/**
+ * LeetCode: 100
+ * Level: Easy
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+   if (p === null && q === null) return true;
+   if ((p && p.val) !== (q && q.val)) return false;
+
+   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
