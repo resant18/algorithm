855789fe7ca424d4dae8d9af8fa4235ba41d9d25
resant18 <santoso.renata@gmail.com>
2020-04-13 11:30:13 -0700

    Add symmetric tree

--- symmetric_tree.js
+++ symmetric_tree.js
@@ -9,4 +9,13 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function (root) {};
+var isSymmetric = function (root) {
+   var isMirror = function (root1, root2) {
+      if (root1 === null && root2 === null) return true;
+      if ((root1 && root1.val) !== (root2 && root2.val)) return false;
+
+      return isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
+   };
+
+   return isMirror(root, root);
+};
