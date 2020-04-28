1199d0e51e2201f711943a50a31113c0ae3387e7
resant18 <santoso.renata@gmail.com>
2020-04-27 17:50:21 -0700

    Add memoization solution

--- tabulation/lib/leet_code_70.js
+++ tabulation/lib/leet_code_70.js
@@ -1,14 +1,33 @@
 // Work through this problem on https://leetcode.com/problems/climbing-stairs/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function climbStairs(n) {
-   let table = new Array(n + 1).fill(0);
-   table[0] = 1;
-   table[1] = 1;
+// This is actually Fibonacci problem
 
-   for (let stair = 2; stair <= n; stair++) {
-      table[stair] = table[stair - 1] + table[stair - 2];
+// function climbStairs(n) {
+//    let table = new Array(n + 1).fill(0);
+//    table[0] = 1;
+//    table[1] = 1;
+
+//    for (let stair = 2; stair <= n; stair++) {
+//       table[stair] = table[stair - 1] + table[stair - 2];
+//    }
+
+//    return table[table.length - 1];
+// }
+
+var climbStairs = function (n, memo = {}) {
+   if (n in memo) return memo[n];
+   if (n === 0) return 1;
+
+   let totalCombination = 0;
+   for (let stair of [1, 2]) {
+      if (n >= stair) {
+         totalCombination += climbStairs(n - stair, memo);          
+      }      
    }
 
-   return table[table.length - 1];
-}
+   memo[n] = totalCombination;
+   return memo[n];
+};
+
+console.log(climbStairs(6));
