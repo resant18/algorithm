bc1c0a5eaf04dc67834eebd5b11cd476f5bc5644
resant18 <santoso.renata@gmail.com>
2020-06-13 15:44:49 -0700

    Add buy sell stock

---
+++ amazon/buy_sell_stock.js
@@ -0,0 +1,53 @@
+/*
+121. Best Time to Buy and Sell Stock
+Level: Easy 
+Company: Amazon, Facebook, Microsoft, Goldman Sachs, Bloomberg, Apple, Oracle, 
+Walmart Labs, Uber, ByteDance, Expedia, Zillow, Atlassian, Audible,
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+PSEUDOCODE:
+Find the minimum and maximum number at one iteration.
+The maximum profit is found by the difference of lowest peak and the highest peak. 
+1. Initiate minimum price (lowest peak) with Max Integer
+2. Initiate maximum profit as zero
+3. Iterate while define the lowest peak and maximum profit:
+   - if price is less than minimum, update the minimum prices
+   - if the difference between price and minimum is greater than maximum profit, update maximum profit
+4. Return maximum profit
+*/
+
+// Time: O(n), Space: O(1)
+const maxProfit = (prices) => 
+{
+   let minPrice = Integer.MAX_VALUE;
+   let maxProfit = 0;
+
+   for (const price of prices) {
+      if (price < minPrice) {
+         minPrice = price
+      } else if (price - minPrice > maxProfit) { // put as else if because we don't want to substract the element with that element itself
+         maxProfit = price - minPrice;
+      }
+   }
+
+   return maxProfit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
