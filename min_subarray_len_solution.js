cd22d831130935cba4277759422b1eadd1b2b7cb
resant18 <santoso.renata@gmail.com>
2020-06-13 16:29:14 -0700

    Add leetcode problem number and company

--- min_subarray_len_solution.js
+++ min_subarray_len_solution.js
@@ -1,3 +1,7 @@
+// LeetCode 209. Minimum Size Subarray Sum
+// Level: Medium
+// Company: Goldman Sachs, Google, Amazon, Oracle, Facebook, Microsoft, SAP
+
 // Given an array of n positive integers and a positive integer s, 
 // find the minimal length of a contiguous subarray of which the sum ≥ s. 
 // If there isn't one, return 0 instead.
