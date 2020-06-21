3a9e83d7f12785ae719878f7da4c3e1efaf9eca5
Renata Santoso <santoso.renata@gmail.com>
2020-06-20 18:58:53 -0700

    Add answer

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -1,9 +1,42 @@
-// View the full problem and run the test cases at:
-//  https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+/*
+LeetCode #105. Construct Binary Tree from Preorder and Inorder Traversal
+Level:Medium
+Company: Amazon,Microsoft,Google,Bloomberg,Facebook,ByteDance,
+
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+*/
 
 const { TreeNode } = require('./tree_node.js');
 
-
 function buildTree(preorder, inorder) {
+   if (preorder.length === 0 || inorder.length === 0) return null;
+   let rootVal = preorder[0];
+   let root = new TreeNode(rootVal);
+   let rootIdx = inorder.indexOf(rootVal);
 
+   let leftInorder = inorder.slice(0, rootIdx);
+   let rightInorder = inorder.slice(rootIdx + 1);
+
+   let leftPreorder = preorder.slice(1, rootIdx + 1);
+   let rightPreorder = preorder.slice(rootIdx + 1);
+
+   root.left = buildTree(leftPreorder, leftInorder);
+   root.right = buildTree(rightPreorder, rightInorder);
+
+   return root;
 }
--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,9 +1,17 @@
 function inOrderArray(root) {
+    if (!root) return;
 
+    inOrderArray(root.left);
+    console.log(root.val);
+    inOrderArray(root.right);
 }
 
 function postOrderArray(root) {
-
+    if (!root) return;
+    
+    inOrderArray(root.left);    
+    inOrderArray(root.right);
+    console.log(root.val);
 }
 
 
