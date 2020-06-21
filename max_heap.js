f6a52f480dccb3714c3c65d69435c9e1d914ac71
Renata Santoso <santoso.renata@gmail.com>
2020-06-21 11:53:44 -0700

    Add HeapSort and isMaxHeap functions

--- max_heap.js
+++ max_heap.js
@@ -12,7 +12,7 @@ class MaxHeap {
    }
 
    getParentIdx(idx) {
-      return Math.floor(idx / 2);      
+      return Math.floor(idx / 2);
    }
 
    getLeftNodeIdx(idx) {
@@ -23,8 +23,9 @@ class MaxHeap {
       return idx * 2 + 1;
    }
 
+   // Time: O(log n) because of siftUp
    insert(val) {
-      // add the new node starting from the bottom level, far-left 
+      // add the new node starting from the bottom level, far-left
       this.array.push(val);
 
       // then, sift that value up the heap to restore heap property
@@ -37,7 +38,7 @@ class MaxHeap {
 
       let parentIdx = this.getParentIdx(idx);
 
-      // if the node is bigger than it's parent: 
+      // if the node is bigger than it's parent:
       if (this.array[idx] > this.array[parentIdx]) {
          // we need to swap them
          [this.array[idx], this.array[parentIdx]] = [this.array[parentIdx], this.array[idx]];
@@ -46,10 +47,11 @@ class MaxHeap {
       }
    }
 
+   // Time: O(log n) because of siftDown
    deleteMax() {
-      // recall that we have an empty position at the very front of the array, 
-      // so an array length of 2 means there is only 1 item in the heap    
-      
+      // recall that we have an empty position at the very front of the array,
+      // so an array length of 2 means there is only 1 item in the heap
+
       // if there are no elements in the heap, do nothing
       if (this.array.length === 1) return;
 
@@ -66,7 +68,7 @@ class MaxHeap {
       return max;
    }
 
-   siftDown(idx) {      
+   siftDown(idx) {
       let leftIdx = this.getLeftNodeIdx(idx);
       let rightIdx = this.getRightNodeIdx(idx);
 
@@ -78,7 +80,7 @@ class MaxHeap {
 
       let max = this.array[idx];
 
-      // if the node is bigger than both children, we have restored heap property, so exit        
+      // if the node is bigger than both children, we have restored heap property, so exit
       if (max > leftVal && max > rightVal) return;
 
       // otherwise the node is bigger than one of it's children,
@@ -87,12 +89,64 @@ class MaxHeap {
          var swapIdx = leftIdx;
       } else {
          var swapIdx = rightIdx;
-      }      
+      }
       [this.array[idx], this.array[swapIdx]] = [this.array[swapIdx], this.array[idx]];
 
       // and continue to sift it down recursively
       this.siftDown(swapIdx);
    }
+}
+
+// Checking if an array is max heap (parent's val > children's val)
+function isMaxHeap(array, idx = 1) {
+   if (array[idx] === undefined) return true;
+
+   let leftIdx = idx * 2;
+   let rightIdx = idx * 2 + 1;
+
+   let leftVal = array[leftIdx] || -Infinity;
+   let rightVal = array[rightIdx] || -Infinity;
+
+   return (array[idx] > leftVal && array[idx] > rightVal) 
+         && isMaxHeap(array, leftIdx)
+         && isMaxHeap(array, rightIdx);
+}
+
+// Sort an array completely in place (mutate the array)
+// Time: O(n log n), Space: O(1)
+function heapSort(array) {
+   for (let i = array.length - 1; i >= 0; i--) {
+      heapify(array, array.length, i);
+   }
+
+   for (let endOfHeap = array.length - 1; endOfHeap >= 0; endOfHeap--) {
+      swap(array, 0, endOfHeap);
+      heapify(array, endOfHeap, 0);
+   }
+   return array;
+}
+
+// Time: O(log n), Space: O(1)
+function heapify(array, n, i) {
+   let leftIdx = i * 2 + 1;
+   let rightIdx = i * 2 + 2;
+   let leftVal = leftIdx >= n ? -Infinity : array[leftIdx];
+   let rightVal = rightIdx >= n ? -Infinity : array[rightIdx];
+
+   let val = array[i];
+   if (val > leftVal && val > rightVal) return;
+   if (leftVal > rightVal) {
+      var swapIdx = leftIdx;
+   }
+   else {
+      var swapIdx = rightIdx;
+   }
+   swap(array, swapIdx, i);
+   heapify(array, n, swapIdx);
+}
+
+function swap(array, i, j) {
+   [array[i], array[j]] = [array[j], array[i]];
 }
 
 let heap = new MaxHeap();
@@ -107,3 +161,10 @@ console.log(heap.array);
 
 console.log(heap.deleteMax());
 console.log(heap.array);
+
+// Heap Sort
+console.log(heapSort([2, 5, 1, 4, 3, 7, 10]));
+
+// Max Heap
+console.log("isMaxHeap: ", isMaxHeap([null, 10, 5, 7, 2, 6])); //false
+console.log("isMaxHeap: ", isMaxHeap([null, 50, 42, 27, 32, 24])); //true
