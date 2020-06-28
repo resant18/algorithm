0515cf8ae120ef1b534845fc79847e05edff02d6
Renata Santoso <santoso.renata@gmail.com>
2020-06-28 09:28:12 -0700

    Add DFS Iterative and recursive

--- tree_traversal.js
+++ tree_traversal.js
@@ -6,15 +6,15 @@ function TreeNode(val) {
 let n1 = new TreeNode(10);
 let n2 = new TreeNode(5);
 let n3 = new TreeNode(15);
-// let n4 = new TreeNode(3);
-// let n5 = new TreeNode(7);
-// let n6 = new TreeNode(18);
+let n4 = new TreeNode(3);
+let n5 = new TreeNode(7);
+let n6 = new TreeNode(18);
 
 n1.left = n2;
 n1.right = n3;
-// n2.left = n4;
-// n2.right = n5;
-// n3.right = n6;
+n2.left = n4;
+n2.right = n5;
+n3.right = n6;
 
 //     10
 //    /  \
@@ -31,9 +31,32 @@ const inOrderRecursive = (root) => {
    inOrder(root.right);
 }
 
-const inOrderIterative = (root) => {
+// DFS Iterative
+const DFSIterative = (root) => {
+   let stack = [root];
 
+   while (stack.length) {
+      let node = stack.pop();
+
+      console.log(node.val);
+      
+      if (node.right) stack.push(node.right);
+      if (node.left) stack.push(node.left);
+   }
 }
+
+// console.log(DFSIterative(n1));
+
+const DFS = (root) => {
+   if (!root) return;
+
+   console.log(root.val);
+
+   DFS(root.left);
+   DFS(root.right);
+}
+
+console.log(DFS(n1));
 
 const BFS = (root) => {   
    let q = [[root, 0]];
