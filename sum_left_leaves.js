e9be21b8793e4abf59477fd061d02b5bcdc437bc
Renata Santoso <santoso.renata@gmail.com>
2020-07-04 18:02:28 -0700

    Update question

--- sum_left_leaves.js
+++ sum_left_leaves.js
@@ -1,3 +1,20 @@
+/*
+LeetCode #404. Sum of Left Leaves
+Level: Easy
+
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+*/
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
