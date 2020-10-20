5de11c8d1db3b05a447f24eeed83c37661a20550
Renata Santoso <santoso.renata@gmail.com>
2020-10-20 14:00:54 -0700

    Add cipher alternative problem

--- caesar_cipher_solution.js
+++ caesar_cipher_solution.js
@@ -28,3 +28,44 @@ function caesarCipher(str, shift) {
 
   return encoded;
 }
+
+
+
+
+function rotationalCipher(input, rotationFactor) {
+  // Write your code here
+  let char_a = "a".charCodeAt(0);
+  let char_z = char_a + 25;
+  let char_A = "A".charCodeAt(0);
+  let char_Z = char_A + 25;
+
+  let cipher = "";
+
+  cipher = input
+      .split("")
+      .map((ch) => {
+        let charCode = ch.charCodeAt(0);
+        let rotatedCode = charCode + rotationFactor;
+        let cipheredCode;
+
+        if (parseInt(ch) >= 0 && parseInt(ch) <= 9) {
+            return ((parseInt(ch) + rotationFactor) % 10).toString();
+        } else if (charCode >= char_a && charCode <= char_z) {
+            cipheredCode = rotatedCode > char_z ? ((rotatedCode - char_a) % 26) + char_a : rotatedCode;
+            return String.fromCharCode(cipheredCode);
+        } else if (charCode >= char_A && charCode <= char_Z) {
+            cipheredCode = rotatedCode > char_Z ? ((rotatedCode - char_A) % 26) + char_A : rotatedCode;
+            return String.fromCharCode(cipheredCode);
+        } else if (parseInt(ch) >= 0 && parseInt(ch) <= 9) {
+            return ((parseInt(ch) + rotationFactor) % 10).toString();
+        } else {
+            return ch;
+        }
+      })
+      .join("");
+  
+  return cipher;
+}
+
+console.log(rotationalCipher("abcdZXYzxy-999.@", 200)); //stuvRPQrpq-999.@
+// abcdZXYzxy-999.@
