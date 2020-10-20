b4cd20b7e219f70c2e4ece9c20373a5462a86ca8
Renata Santoso <santoso.renata@gmail.com>
2020-10-20 11:49:41 -0700

    Add alternative problem that consider duplicate

--- two_sum_solution.js
+++ two_sum_solution.js
@@ -86,4 +86,58 @@ function stockPairs(stocksProfit, target) {
    return totalPairs;
 }
 
-console.log(stockPairs([7, 6, 6, 3, 9, 3, 5, 1 ], 12));
+// console.log(stockPairs([7, 6, 6, 3, 9, 3, 5, 1 ], 12));
+
+// Two sum counting duplicate element, as long as the index is different, it should be consider as a pair.
+function numberOfWays(arr, k) {
+   // Write your code
+   let output = 0;
+   let map = {};   
+
+   for (let i = 0; i < arr.length; i++) {
+      let pair1 = arr[i];
+      let pair2 = k - arr[i];
+
+      if (pair2 in map) output += map[pair2];
+      map[pair1] = map[pair1] + 1 || 1;
+   }
+
+   return output;
+}
+
+console.log(numberOfWays2([1, 1, 2, 7, 6, 1, 2, 3, 4, 5, 5], 6));
+
+// Two Pointers
+function numberOfWays2(arr, k) {   
+   let output = 0;
+   let left = 0;
+   let right = arr.length - 1;
+
+   arr.sort((a, b) => a - b);
+   while (left < right) {
+      if (arr[left] + arr[right] < k) {
+         left++;
+      } else if (arr[left] + arr[right] > k) {
+         right--;
+      } else {
+         let start = arr[left];
+         let num1 = 0; 
+         
+         while (start === arr[left]) {
+            num1++;
+            left++;
+         }
+         let end = arr[right];
+         let num2 = 0;
+         while (end === arr[right]) {
+            num2++;
+            right--;
+         }
+         output += (num1 * num2);
+      }
+   }
+
+   return output;
+}
+
+console.log(numberOfWays2([1, 1, 2, 7, 6, 1, 2, 3, 4, 5, 5], 6));
