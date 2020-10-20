7b97c030017241366e847b39f5091224505ef184
Renata Santoso <santoso.renata@gmail.com>
2020-10-20 15:48:14 -0700

    Refactor solution

--- anagrams_solution.js
+++ anagrams_solution.js
@@ -6,18 +6,40 @@
 // Examples:
 // anagrams('listen', 'silent') => true
 // anagrams('listen', 'potato') => false
+// function anagrams(str1, str2) {
+//   const letters = {};
+
+//   str1.split("").forEach(char => {
+//     if (!letters[char]) letters[char] = 0;
+//     letters[char] += 1;
+//   });
+
+//   str2.split("").forEach(char => {
+//     if (!letters[char]) letters[char] = 0;
+//     letters[char] -= 1;
+//   });
+
+//   return Object.values(letters).every(letterCount => letterCount === 0);
+// }
+
+// Time: O(n), Space: O(n)
 function anagrams(str1, str2) {
-  const letters = {};
+   const letters = {};
 
-  str1.split("").forEach(char => {
-    if (!letters[char]) letters[char] = 0;
-    letters[char] += 1;
-  });
+   str1.split("").forEach((char) => {
+      letters[char] = letters[char] + 1 || 1;
+   });
 
-  str2.split("").forEach(char => {
-    if (!letters[char]) letters[char] = 0;
-    letters[char] -= 1;
-  });
+   console.log(letters);
 
-  return Object.values(letters).every(letterCount => letterCount === 0);
+   str2.split("").forEach((char) => {
+      if (!(char in letters)) letters[char] = 1;
+      else letters[char] -= 1;
+   });
+
+   console.log(letters);
+
+   return Object.values(letters).every((letterCount) => letterCount === 0);
 }
+
+console.log(anagrams("listen", "potato"));
