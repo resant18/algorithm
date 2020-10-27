2b6c39915e6d6ca151aa7c751fd2241c1afe231c
Renata Santoso <santoso.renata@gmail.com>
2020-10-27 14:44:23 -0700

    Add stack with minMax

---
+++ minmax_stack.js
@@ -0,0 +1,99 @@
+// Feel free to add new properties and methods to the class.
+class MinMaxStack {
+   constructor() {
+      this.stack = [];
+      this.minMax = [];
+   }
+
+   // Time: O(1), Space: O(1)
+   peek() {
+      // Write your code here.
+      return this.stack[this.stack.length - 1];
+   }
+
+   // Time: O(1), Space: O(1)
+   pop() {
+      // Write your code here.
+      this.minMax.pop();
+      return this.stack.pop();
+   }
+
+   // Time: O(1), Space: O(1)
+   push(number) {
+      // Write your code here.
+      let lastMinMax = this.minMax[this.minMax.length - 1];
+      if (this.stack.length === 0) {
+         this.minMax.push({
+            min: number,
+            max: number,
+         });
+      } else {
+         this.minMax.push({
+            min: Math.min(lastMinMax.min, number),
+            max: Math.max(lastMinMax.max, number),
+         });
+      }
+      this.stack.push(number);
+   }
+
+   // Time: O(1), Space: O(1)
+   getMin() {
+      // Write your code here.
+      return this.minMax[this.minMax.length - 1].min;
+   }
+
+   // Time: O(1), Space: O(1)
+   getMax() {
+      // Write your code here.
+      return this.minMax[this.minMax.length - 1].max;
+   }
+
+   // Time: O(1), Space: O(1)
+   isEmpty() {
+      return this.stack.length === 0;
+   }
+
+   // Time: O(N), Space: O(1)
+   reverse() {
+      // 1, 2, 3, 4
+
+      prev = 1 
+      curr = 2
+      next = 3
+   }
+
+   print() {
+      for (const item of this.stack) {
+         console.log(item);
+      }
+   }
+}
+
+let m = new MinMaxStack();
+m.push(5);
+// console.log(m.getMin());
+// console.log(m.getMax());
+m.push(4);
+m.push(2);
+m.push(1);
+
+m.print();
+
+m.reverse();
+
+m.print();
+
+1 -> 2 -> 3 -> 4 -> null
+
+prev = null 
+curr = 1
+next 2 
+
+next = curr.next;
+curr.next = prev
+
+prev = curr;
+curr = next;
+
+
+
