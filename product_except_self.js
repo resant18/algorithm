f8651c236f4e78dfcf647c67ddf0e5c1f7493bdf
Renata Santoso <santoso.renata@gmail.com>
2020-11-29 20:42:55 -0800

    Update variable name

--- product_except_self.js
+++ product_except_self.js
@@ -72,10 +72,10 @@ const productExceptSelf2 = (nums) => {
       result[i] = result[i - 1] * nums[i - 1];
    }
 
-   let temp = 1;
+   let productOfRight = 1;
    for (let i = n - 1; i >= 0; i--) {
-      result[i] = result[i] * temp;
-      temp *= nums[i];
+      result[i] = result[i] * productOfRight;
+      productOfRight *= nums[i];
    }
 
    return result;
