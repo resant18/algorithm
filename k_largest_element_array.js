8e3ec14359c827e4fff156747f220a9b0a0e49c6
Renata Santoso <santoso.renata@gmail.com>
2020-11-29 17:37:11 -0800

    Add alternative solution

--- k_largest_element_array.js
+++ k_largest_element_array.js
@@ -25,26 +25,28 @@ var findKthLargest = function (nums, k) {
    return nums.sort((a, b) => b - a)[k - 1];   
 };
 
-console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
-console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+console.log("Method 1: Using built-in sorting");
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); //5
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); //4
 
-// Method 2: Max Heap
-// Time: O(n log n, but faster
+// ==================================================================
+// Method 2: Using Max Heap
+// Time: O(n + k Logn)
 class MaxHeap {
    constructor() {
-      this.array = [null];
+      this.array = [];
    }
 
    getParentIdx(idx) {
-      return Math.floor(idx / 2);
+      return Math.floor((idx - 1 ) / 2);
    }
 
    getLeftNodeIdx(idx) {
-      return idx * 2;
+      return idx * 2 + 1;
    }
 
    getRightNodeIdx(idx) {
-      return idx * 2 + 1;
+      return idx * 2 + 2;
    }
 
    // Time: O(log n) because of siftUp
@@ -53,59 +55,56 @@ class MaxHeap {
       this.array.push(val);
 
       // then, sift that value up the heap to restore heap property
-      this.siftUp(this.array.length - 1);
+      this.siftUp(this.array.length - 1, this.array);
    }
 
-   siftUp(idx) {
+   siftUp(idx, array) {
       // if the node is already at the root, there's no further we can sift up
-      if (idx === 1) return;
+      if (idx === 0) return;
 
       let parentIdx = this.getParentIdx(idx);
 
-      // if the node is bigger than it's parent:
-      if (this.array[idx] > this.array[parentIdx]) {
+      // if the node is greater than it's parent:
+      if (array[idx] > array[parentIdx]) {
          // we need to swap them
-         [this.array[idx], this.array[parentIdx]] = [this.array[parentIdx], this.array[idx]];
+         [array[idx], array[parentIdx]] = [array[parentIdx], array[idx]];
          // and continue to sift it up recursively
-         this.siftUp(parentIdx);
+         this.siftUp(parentIdx, array);
       }
    }
 
    // Time: O(log n) because of siftDown
    deleteMax() {
-      // recall that we have an empty position at the very front of the array,
-      // so an array length of 2 means there is only 1 item in the heap
-
       // if there are no elements in the heap, do nothing
-      if (this.array.length === 1) return;
+      if (this.array.length < 1) return;
 
       // if there is only 1 element in the heap, simply remove it
-      if (this.array.length === 2) return this.array.pop();
+      if (this.array.length === 1) return this.array.pop();
 
       // otherwise remove the last element and make it the root at the front of the array
-      let max = this.array[1];
-      this.array[1] = this.array.pop();
+      [this.array[0], this.array[this.array.length - 1]] = [this.array[this.array.length - 1], this.array[0]];
+      let max = this.array.pop();
 
       // then, sift the new root down to restore heap property
-      this.siftDown(1);
+      this.siftDown(0, this.array);
 
       return max;
    }
 
-   siftDown(idx) {
+   siftDown(idx, array) {
       let leftIdx = this.getLeftNodeIdx(idx);
       let rightIdx = this.getRightNodeIdx(idx);
 
       // if the node is missing children, consider the missing children as the value -Infinity
       // this allows the node to keep heap property, since any value is greater than -Infinity
       // this will also give us children values to compare later, undefined should not be used for comparison**
-      let leftVal = this.array[leftIdx] || -Infinity;
-      let rightVal = this.array[rightIdx] || -Infinity;
+      let leftVal = array[leftIdx] || -Infinity;
+      let rightVal = array[rightIdx] || -Infinity;
 
-      let max = this.array[idx];
+      let curVal = array[idx];
 
-      // if the node is bigger than both children, we have restored heap property, so exit
-      if (max > leftVal && max > rightVal) return;
+      // if the node is greater than both children, we have restored heap property, so exit
+      if (curVal > leftVal && curVal > rightVal) return;
 
       // otherwise the node is bigger than one of it's children,
       // so swap this node with the bigger between the two children**
@@ -114,17 +113,147 @@ class MaxHeap {
       } else {
          var swapIdx = rightIdx;
       }
-      [this.array[idx], this.array[swapIdx]] = [this.array[swapIdx], this.array[idx]];
+      [array[idx], array[swapIdx]] = [array[swapIdx], array[idx]];
 
       // and continue to sift it down recursively
-      this.siftDown(swapIdx);
+      this.siftDown(swapIdx, array);
+   }
+
+   size() {
+      return this.array.length;
+   }
+
+   buildHeap(array) {
+      let firstParentIdx = this.getParentIdx(array.length - 1);
+
+      for (let i = firstParentIdx; i >= 0; i--) {
+         this.siftDown(i, array);
+      }
+      this.array = array;
    }
 }
 
-var findKthLargest = function (nums, k) {
-   let heap = new MaxHeap();
-   nums.forEach((num) => heap.insert(num));
-   for (let i = 1; i < k; i++) heap.deleteMax();
-   return heap.deleteMax();
+var findKthLargestWithMaxHeap = function (nums, k) {
+   let maxHeap = new MaxHeap();
+   maxHeap.buildHeap(nums);
+   
+   for (let i = 0; i < k - 1; i++) {
+      maxHeap.deleteMax();
+   }
+
+   return maxHeap.deleteMax();
 };
 
+console.log("Method 2: Using Max Heap")
+console.log(findKthLargestWithMaxHeap([3, 2, 1, 5, 6, 4], 2)); //5
+console.log(findKthLargestWithMaxHeap([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); //4
+
+// ==================================================================
+// Method 3: Using Min Heap
+// Time: O(k + (n-k) * Logk) => faster
+class MinHeap {
+   constructor() {
+      this.array = [];
+   }
+
+   getParentIdx(idx) {
+      return Math.floor((idx - 1 ) / 2);
+   }
+
+   getLeftNodeIdx(idx) {
+      return idx * 2 + 1;
+   }
+
+   getRightNodeIdx(idx) {
+      return idx * 2 + 2;
+   }
+
+   // Time: O(log n) because of siftUp
+   insert(val) {
+      // add the new node starting from the bottom level, far-left
+      this.array.push(val);
+
+      // then, sift that value up the heap to restore heap property
+      this.siftUp(this.array.length - 1, this.array);
+   }
+
+   siftUp(idx, array) {
+      // if the node is already at the root, there's no further we can sift up
+      if (idx === 0) return;
+
+      let parentIdx = this.getParentIdx(idx);
+
+      // if the node is lesser than it's parent:
+      if (array[idx] < array[parentIdx]) {
+         // we need to swap them
+         [array[idx], array[parentIdx]] = [array[parentIdx], array[idx]];
+         // and continue to sift it up recursively
+         this.siftUp(parentIdx, array);
+      }
+   }
+
+   // Time: O(log n) because of siftDown
+   deleteMin() {
+      // if there are no elements in the heap, do nothing
+      if (this.array.length < 1) return;
+
+      // if there is only 1 element in the heap, simply remove it
+      if (this.array.length === 1) return this.array.pop();
+
+      // otherwise remove the last element and make it the root at the front of the array
+      [this.array[0], this.array[this.array.length - 1]] = [this.array[this.array.length - 1], this.array[0]];
+      let min = this.array.pop();
+
+      // then, sift the new root down to restore heap property
+      this.siftDown(0, this.array);
+
+      return min;
+   }
+
+   siftDown(idx, array) {
+      let leftIdx = this.getLeftNodeIdx(idx);
+      let rightIdx = this.getRightNodeIdx(idx);
+
+      // if the node is missing children, consider the missing children as the value -Infinity
+      // this allows the node to keep heap property, since any value is greater than -Infinity
+      // this will also give us children values to compare later, undefined should not be used for comparison**
+      let leftVal = array[leftIdx] || Infinity;
+      let rightVal = array[rightIdx] || Infinity;
+
+      let curVal = array[idx];
+
+      // if the node is lesser than both children, we have restored heap property, so exit
+      if (curVal < leftVal && curVal < rightVal) return;
+
+      // otherwise the node is bigger than one of it's children,
+      // so swap this node with the bigger between the two children**
+      if (leftVal < rightVal) {
+         var swapIdx = leftIdx;
+      } else {
+         var swapIdx = rightIdx;
+      }
+      [array[idx], array[swapIdx]] = [array[swapIdx], array[idx]];
+
+      // and continue to sift it down recursively
+      this.siftDown(swapIdx, array);
+   }
+
+   size() {
+      return this.array.length;
+   }
+}
+
+var findKthLargestWithMinHeap = function (nums, k) {
+   let minHeap = new MinHeap();
+   nums.forEach((num) => {
+      minHeap.insert(num);      
+      if (minHeap.size() > k) minHeap.deleteMin();      
+   });
+   return minHeap.deleteMin();
+};
+
+console.log("Method 3: Using Min Heap");
+console.log(findKthLargestWithMinHeap([3, 2, 1, 5, 6, 4], 2)); //5
+console.log(findKthLargestWithMinHeap([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); //4
+
+// Method 4: Using Quick Select
