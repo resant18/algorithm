09293037b2122d27b459bfcc204c6183138fdbae
Renata Santoso <santoso.renata@gmail.com>
2020-12-02 10:46:13 -0800

    Add bookmark chapter book

---
+++ bookmark_chapter_book.js
@@ -0,0 +1,17 @@
+/*
+Glassdoor
+Company: Audible
+	
+Given an array with starting page numbers of different chapters(say ch) 
+and another array which had all the page numbers that had been bookmarked by a user(say b), 
+find the bookmarks in a given list of chapters
+
+Example:
+Input:
+ch = [0, 10, 13, 36, 59, 70]
+b = [23, 44, 66, 69]
+Output: should be 23 and 44.  
+Chapter 1 starts at page number 0, and ch1 from 10 and so on.
+Find the bookmarks in chapters 1,3,4
+*/
+
